f481350749c77986cc10404e1a641fa7663aa565
kuniterukbo <r2ka2h5e@icloud.com>
2021-02-16 22:26:10 +0900

    問題の取り出し

--- js/main.js
+++ js/main.js
@@ -2,7 +2,15 @@ $(function(){
   let char_index = 1;
   let max_length = 3;
   let question_number = 1;
-
+  const $yomi = $("#yomi");
+  const $mondai = $("#mondai");
+  const mondai_list = [
+    {yomi: "ごはん", text:"gohan"},
+    {yomi: "おすし", text:"osushi"},
+    {yomi: "サイフ", text:"saifu"},
+    {yomi: "バナナ", text:"banana"},
+    {yomi: "くつした", text:"kutusita"},
+  ];
   $(document).on("keypress",function(e){
     const $target = $("#char-"+char_index);
     const char = $target.text();
@@ -19,7 +27,8 @@ $(function(){
   }
 
   function changeQuestionWord() {
+    $yomi.text(mondai_list[question_number]["yomi"]);
 
   }
-  
+
 });
